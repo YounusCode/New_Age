7097ffcf8568488bc8527bea8237103696dcb116
Younus Ali <younus_ali45@icloud.com>
2023-08-23 03:46:14 -0500

    happy_birthday.py

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,6 +1,13 @@
 def happy_birthday(name, year_born, current_year):
     new_age = current_year - year_born
-    print(f"Happy {new_age}th Birthday, {name}!")
+    suffix = "th"
+    if new_age % 10 == 1 and new_age % 100 != 11:
+        suffix = "st"
+    elif new_age % 10 == 2 and new_age % 100 != 12:
+        suffix = "nd"
+    elif new_age % 10 == 3 and new_age % 100 != 13:
+        suffix = "rd"
+    print(f"Happy {new_age}{suffix} Birthday, {name}!")
 
 
 # Example of calling the function
