a049b6f4f3f1f55d67f68b85648a03a26937ce11
Younus Ali <younus_ali45@icloud.com>
2023-08-23 03:33:19 -0500

    happy_birthday.py

---
+++ happy_birthday.py
@@ -0,0 +1,7 @@
+def happy_birthday(name, year_born, current_year):
+    new_age = current_year - year_born
+    print(f"Happy {new_age}th Birthday, {name}!")
+
+
+# Example of calling the function
+happy_birthday('Emily', 1906, 1944)
